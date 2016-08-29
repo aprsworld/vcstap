4ed077160114a30e5b85d38f822b8531fffce3b5
James Jarvis <jj@aprsworld.com>
2016-08-29 14:24:26 -0500

    Hopefully last endian try

--- live.c
+++ live.c
@@ -68,7 +68,7 @@ void live_send_vcs(void) {
 	/* any fault */
 //	if ( timer.vcs_register[30*4] || timer.vcs_register[30*4+1] || timer.vcs_register[30*4+2] || timer.vcs_register[30*4+3] ) {
 	/* system state 6 (FAULT) */
-	if ( 6==timer.vcs_register[19*4+1] && 0==timer.vcs_register[19*4+0] && 0==timer.vcs_register[19*4+3] && 0==timer.vcs_register[19*4+2] ) {
+	if ( 6==timer.vcs_register[19*4+3] && 0==timer.vcs_register[19*4+2] && 0==timer.vcs_register[19*4+1] && 0==timer.vcs_register[19*4+0] ) {
 		output_high(RELAY_RED);
 	} else {
 		output_low(RELAY_RED);
